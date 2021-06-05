e4e64e57a804a5e576f8b7eaeef80084d9e62f8a
BiZhiChen01 <1973626642@qq.com>
2021-06-05 09:37:31 +0800

    完成新书页面

--- src/views/NewBooks.vue
+++ src/views/NewBooks.vue
@@ -1,17 +1,44 @@
 <template>
     <div class="new-books">
         <div class="container">
-            NewBooks Page
+            <BookList :data="booksData" />
         </div>
     </div>
 </template>
 
 <script>
+import api from '../services';
+
+import BookList from '../components/BooksList';
+
 export default {
-    name: 'NewBooksPage'
+    name: 'NewBooksPage',
+    components: {
+        BookList
+    },
+    data() {
+        return {
+            booksData: []
+        }
+    },
+    async mounted() {
+        await api.getBooks().then((result) => {
+            if (result.status === 200) {
+                this.booksData = result.data;
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+    width: 1120px;
+    padding: 10px;
+    background-color: #fff;
+    box-shadow: 0 5px 10px #d4d4d4;
+    overflow: hidden;
+}
 </style>
